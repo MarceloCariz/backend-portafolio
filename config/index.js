ef28d133366bdd1b0450fa63eef8052fcf5eb3e2
MarceloCariz <marcejedd300@hotmail.com>
2022-08-13 15:53:37 -0400

    ruta directa wallet

--- config/index.js
+++ config/index.js
@@ -1,8 +1,20 @@
 import oracledb from 'oracledb';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+const instantClient =path.resolve(__dirname,'instantclient_21_6');
+const wallet =path.resolve(__dirname,'Wallet_MAIPOGRANDE');
+
+console.log(wallet)
 // oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
-import path from 'path';
-oracledb.initOracleClient({libDir: 'C:\\Users\\marce\\OneDrive\\Escritorio\\PORTAFOLIO\\instantclient_21_6'});
-const cs = 'tcps://adb.sa-saopaulo-1.oraclecloud.com:1522/g4398818274a3ca_maipogrande_high.adb.oraclecloud.com?wallet_location=C:\\Users\\marce\\OneDrive\\Escritorio\\PORTAFOLIO\\backend\\config\\Wallet_MAIPOGRANDE&retry_count=20&retry_delay=3'
+// 'C:\\Users\\Default.LAPTOP-V3GJG796\\Desktop\\PORTAFOLIO\\backend\\backend-portafolio\\config\\instantclient_21_6'
+import path, { dirname } from 'path';
+oracledb.initOracleClient({libDir:  instantClient});
+const cs = `tcps://adb.sa-saopaulo-1.oraclecloud.com:1522/g4398818274a3ca_maipogrande_high.adb.oraclecloud.com?wallet_location=${wallet}&retry_count=20&retry_delay=3`
 console.log(path.join("/config/Wallet_MAIPOGRANDE"))
 const conectarDB = async() =>{
     let conexion;
