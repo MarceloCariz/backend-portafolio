7288598c1a4ec3b2ce17351a2345d8743bb39de0
MarceloCariz <marcejedd300@hotmail.com>
2022-08-13 16:54:56 -0400

    client linux

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 
-const instantClient =path.resolve(__dirname,'instantclient_21_6');
+const instantClient =path.resolve(__dirname,'instantclient_21_7');
 const wallet =path.resolve(__dirname,'Wallet_MAIPOGRANDE');
 
 console.log(wallet)
